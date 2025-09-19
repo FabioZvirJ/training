3b473187fc6b5c4b588e79385ccb9697fddb556f
Fabio RIcardo Zvir Junior <zvirfabiojr88@gmail.com>
2025-09-19 15:57:59 -0300

    FEAT: CREATING A NEW PAGE TEXT

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:training/text_widget.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -23,11 +24,4 @@ class GradientContainer extends StatelessWidget {
   }
 }
 
-class TextWidget extends StatelessWidget {
-  const TextWidget({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Text('Teste', style: TextStyle(color: Colors.white, fontSize: 28));
-  }
-}
