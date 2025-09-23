eee96309c49784c5dfc7aed23ab317c00fb34fe3
Fabio RIcardo Zvir Junior <zvirfabiojr88@gmail.com>
2025-09-23 15:03:29 -0300

    FEAT: Insert multi-variable for gradient colors

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,16 +5,19 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.firstColor, required this.secondColor});
+
+  final Color firstColor;
+  final Color secondColor;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
+            firstColor,
+            secondColor
           ],
           begin: startAlignment,
           end: endAlignment,
--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(firstColor: Color.from(alpha: 0.2, red: 0, green: 0.078, blue: 0.286), secondColor: Color.fromARGB(51, 2, 69, 255),),
       ),
     ),
   );
