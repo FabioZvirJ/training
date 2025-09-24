0414661067ed68e58e95596adc716f33a9467383
Fabio RIcardo Zvir Junior <zvirfabiojr88@gmail.com>
2025-09-23 22:40:40 -0300

    FEAT: add new style for to the button

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,33 +5,44 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key, required this.firstColor, required this.secondColor});
+  const GradientContainer({
+    super.key,
+    required this.firstColor,
+    required this.secondColor,
+  });
 
   final Color firstColor;
   final Color secondColor;
+
+  void rollDice() {}
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            firstColor,
-            secondColor
-          ],
+          colors: [firstColor, secondColor],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
       child: Center(
-        child: Column(children: [
-          Image.asset('assets/images/dice-2.png', width: 300),
-          TextButton(onPressed: (), child: const Text('Roll Dice'))
-        ],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/dice-2.png', width: 300),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.only(top: 20,),
+                foregroundColor: Colors.white,
+                textStyle: TextStyle(fontSize: 28),
+              ),
+              child: const Text('Roll Dice'),
+            ),
+          ],
+        ),
       ),
-      )
     );
   }
 }
-
-
